45f39bd9c5bb7ed8cf03594baf7ef678642b0ba4
Sophia <116626295+sofiyakamalova@users.noreply.github.com>
2023-11-21 14:18:12 +0600

    successful commit

---
+++ topic7_iterables.dart
@@ -0,0 +1,66 @@
+import 'dart:io';
+
+void main() {
+  /*
+  //TASK 1
+
+  Iterable<int> spisok = [1, 2, 3, 4, 5];
+  int product = spisok.reduce((value, element) => value *= element);
+  print(product);
+
+
+  //TASK 2
+
+  Iterable<int> spisok2 = [1, -2, 3, 4, 56, 7];
+  int max =
+      spisok2.reduce((value, element) => value > element ? value : element);
+  int min =
+      spisok2.reduce((value, element) => value < element ? value : element);
+  print(max);
+  print(min);
+
+
+  //TASK 4
+
+  Iterable<int> spisok3 = [1, -2, 3, -4, 5, -6, 7, 8, -9, -10];
+  var new_spisok = spisok3.where((element) => element < 0);
+  print(new_spisok);
+
+
+
+  //TASK 7
+
+  int n = int.parse(stdin.readLineSync()!);
+
+  List<dynamic> spisok7 = [];
+
+  for (int i = 1; i < n; i++) {
+    spisok7.add(i);
+
+    if (i % 3 == 0) {
+      spisok7.remove(i);
+      spisok7.add('Fizz');
+    } else if (i % 5 == 0) {
+      spisok7.remove(i);
+      spisok7.add('Buzz');
+    } else if (i % 3 == 0 && i % 5 == 0) {
+      spisok7.remove(i);
+      spisok7.add('FizzBuzz');
+    }
+  }
+  print(spisok7); 
+  
+  
+  //TASK 13
+
+  Iterable<int> spisok13 = [13, 25, 38, 24, 50, 16, 73, 89];
+  int max =
+      spisok13.reduce((value, element) => value > element ? value : element);
+  int min =
+      spisok13.reduce((value, element) => value < element ? value : element);
+
+  int difference = max - min;
+  print('The maximum difference is $difference in a pair $max with $min .');
+
+*/
+}
